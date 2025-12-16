e4b974f61bca82fb5fa90bf246cbe2bdd2dc9581
peupeuls <msona6261@gmail.com>
2025-12-17 04:25:52 +0500

    готовое задание про бешеные холодильники

--- stepik.py
+++ stepik.py
@@ -6,12 +6,25 @@ for i in range (0, n):
 
 b = len(slova)
 for i in range (0, b):
-    if 'a' in slova[i]:
-        if 'n' in slova[i]:
-            if 't' in slova[i]:
-                if 'o' in slova[i]:
-                    if 'n' in slova[i]:
-                        sch.append(i+1)
-
+    slovechko = list(slova[i])
+    c = len(slovechko)
+    found = False
+    for j in range (0, c):
+        if slovechko[j] == 'a':
+            for k in range (j+1, c):
+                if slovechko[k] == 'n':
+                    for l in range (k+1, c):
+                        if slovechko[l] == 't':
+                            for m in range (l+1, c):
+                                if slovechko[m] == 'o':
+                                    for z in range (m+1, c):
+                                        if slovechko[z] == 'n':
+                                            sch.append(i+1)
+                                            found = True
+                                            break
+                                    if found: break
+                            if found: break
+                    if found: break
+        if found: break
 result = " ".join(str(x) for x in sch)
 print(result)
