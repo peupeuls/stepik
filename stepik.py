ec8a7d8c2b864c4407b2bf3260abbe8ab3988cda
peupeuls <msona6261@gmail.com>
2025-12-18 03:01:04 +0500

    считает сумму всех чисел и делит на их количество

--- stepik.py
+++ stepik.py
@@ -1,8 +1,12 @@
-list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
-maximum = 0
-for sublist in list1:
-    proverka = max(sublist)
-    if proverka > maximum:
-        maximum = max(sublist)
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
 
-print(maximum)
+total = 0
+counter = 0
+for sublist in list1:
+    n = len(sublist)
+    counter += n
+    for i in range (0, n):
+        total += sublist[i]
+ 
+otvet=total/counter
+print(otvet)
