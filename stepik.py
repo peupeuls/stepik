0a2cb0af3f718feff5b761a7e4bf843273f54639
peupeuls <msona6261@gmail.com>
2025-12-18 02:45:54 +0500

    нахождение наибольшего элемента во вложенных списках

--- stepik.py
+++ stepik.py
@@ -1,30 +1,8 @@
-n = int(input())
-slova = list()
-sch = list()
-for i in range (0, n):
-    slova.append(str(input()))
+list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+maximum = 0
+for sublist in list1:
+    proverka = max(sublist)
+    if proverka > maximum:
+        maximum = max(sublist)
 
-b = len(slova)
-for i in range (0, b):
-    slovechko = list(slova[i])
-    c = len(slovechko)
-    found = False
-    for j in range (0, c):
-        if slovechko[j] == 'a':
-            for k in range (j+1, c):
-                if slovechko[k] == 'n':
-                    for l in range (k+1, c):
-                        if slovechko[l] == 't':
-                            for m in range (l+1, c):
-                                if slovechko[m] == 'o':
-                                    for z in range (m+1, c):
-                                        if slovechko[z] == 'n':
-                                            sch.append(i+1)
-                                            found = True
-                                            break
-                                    if found: break
-                            if found: break
-                    if found: break
-        if found: break
-result = " ".join(str(x) for x in sch)
-print(result)
+print(maximum)
