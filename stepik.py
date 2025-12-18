d5ddd86756664a247ab075207bf9340d47690f57
peupeuls <msona6261@gmail.com>
2025-12-19 00:28:41 +0500

    нахождение коэффициентов в n строке треугольника Паскаля

--- stepik.py
+++ stepik.py
@@ -1,8 +1,18 @@
+def fact(n):
+    a = 1
+    if n == 0:
+        return 1
+    while n >= 1:
+        a *= n
+        n -= 1
+    return a
+
+
+def paskal(n):
+    c = []
+    for k in range(n+1):
+        c.append(int(fact(n)/(fact(k)*fact(n-k))))
+    return c
+
 n = int(input())
-my_list = []
-for i in range(n):
-    my_list.append(i+1)
-for i in range(n):
-    print(my_list)
-
-
+print(paskal(n))
