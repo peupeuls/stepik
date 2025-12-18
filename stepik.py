0faeee2f38db3f0f4eb7ca9626ece9da9da1c0f6
peupeuls <msona6261@gmail.com>
2025-12-19 01:50:24 +0500

    принимает на вход список и число, задающее размер чанка (куска), а возвращает список из чанков (кусков) указанной длины.

--- stepik.py
+++ stepik.py
@@ -1,11 +1,14 @@
-stroka = input().split()
-n = len(stroka)
-my_list = []
-my_list.append([stroka[0]])
-for i in range(1, n):
-    if stroka[i] != stroka [i-1]:
-        my_list.append([stroka[i]])
-    else:
-        my_list[-1].append(stroka[i])
 
-print(my_list)
+def chunked():
+    stroka = input().split()
+    m = int(input())
+    n = len(stroka)
+    my_list = []
+    my_list.append([stroka[0]])
+    for i in range(1, n):
+        if i % m != 0:
+            my_list[-1].append(stroka[i])
+        else:
+            my_list.append([stroka[i]])
+    print(my_list)
+chunked()
