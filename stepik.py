a7769c67e6461e21b30ffb287c5ed81425c3ad9b
peupeuls <msona6261@gmail.com>
2025-12-18 13:49:08 +0500

    На вход программе подается число n . Напишите программу, которая создает и выводит построчно список, состоящий из n  списков

--- stepik.py
+++ stepik.py
@@ -1,12 +1,8 @@
-list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+n = int(input())
+my_list = []
+for i in range(n):
+    my_list.append(i+1)
+for i in range(n):
+    print(my_list)
 
-total = 0
-counter = 0
-for sublist in list1:
-    n = len(sublist)
-    counter += n
-    for i in range (0, n):
-        total += sublist[i]
- 
-otvet=total/counter
-print(otvet)
+
