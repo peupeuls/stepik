d8f738596ea0c0eff5f08d05addd851ed5cd9bc2
peupeuls <msona6261@gmail.com>
2025-12-22 01:45:31 +0500

    Максимальный в области 2 🌶️ (область бантиком)

--- stepik.py
+++ stepik.py
@@ -3,27 +3,15 @@ s=[]
 for i in range(n):
     s.append(str(input()).split())
 
-maximum1 = 0
-proverka1 = 0
-for i in range(n):
-    for j in range(i+1):
-     if (i >= j) &  (i + j + 1 <= n):
-      proverka1 = int(s[i][j])
-      if maximum1 == 0 or maximum1 < proverka1:
-         maximum1 = proverka1
-
-
-
-maximum2 = 0
-proverka2 = 0
+maximum = 0
+proverka = 0
 for i in range(n):
     for j in range(n):
-     if (i <= j) & (i + j + 1 >= n):
-      proverka2 = int(s[i][j])
-      if maximum2 == 0 or maximum2 < proverka2:
-         maximum2 = proverka2
+     if (i >= j) &  (i + j + 1 <= n) | (i <= j) & (i + j + 1 >= n):
+      proverka = int(s[i][j])
+      if maximum == 0 or maximum < proverka:
+         maximum = proverka
 
-maximum = max(maximum1, maximum2)
+
 print(maximum)
-
         
