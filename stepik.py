1f9223a3ebc566e2f8bb2e4102b7086b8e52b621
peupeuls <msona6261@gmail.com>
2025-12-22 02:11:21 +0500

    Создайте матрицу mult размером n × m  и заполните ее таблицей умножения по формуле mult[i][j] = i * j.

--- stepik.py
+++ stepik.py
@@ -1,24 +1,9 @@
 n = int(input())
-s=[]
-for i in range(n):
-    s.append(str(input()).split())
+m = int(input())
 
-levaya = 0
-pravaya = 0
-verhnaya = 0
-nizhnaya = 0
+mult = [[0] * m for _ in range(n)]
 for i in range(n):
-    for j in range(n):
-     if (i > j) &  (i + j + 1 < n):
-        levaya += int(s[i][j])
-     if (i < j) & (i + j + 1 > n):
-         pravaya += int(s[i][j])
-     if (i < j) &  (i + j + 1 < n):
-         verhnaya += int(s[i][j])
-     if (i > j) &  (i + j + 1 > n):
-         nizhnaya += int(s[i][j])
-         
-print ("Верхняя четверть: " + str(verhnaya) + '\n' + 
-"Правая четверть: " + str(pravaya) + '\n' +
-"Нижняя четверть: " + str(nizhnaya) + '\n' +
-"Левая четверть: " + str(levaya))
+   for j in range(m):
+      mult[i][j] = i * j
+      print(mult[i][j], end = ' ')
+   print()
