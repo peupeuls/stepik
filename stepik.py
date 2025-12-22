de77f953f278be6a5c8de5410675f4500c8197ac
peupeuls <msona6261@gmail.com>
2025-12-23 00:35:47 +0500

    находит индексы (строку и столбец) первого вхождения максимального элемента.

--- stepik.py
+++ stepik.py
@@ -1,9 +1,28 @@
 n = int(input())
 m = int(input())
+s=[]
+for i in range(n):
+    s.append(str(input()).split())
 
-mult = [[0] * m for _ in range(n)]
+maximum = 0
+proverka = 0
+mnogo = []
+for i in range(n):
+    for j in range(m):
+      proverka = int(s[i][j])
+      if maximum == 0 or maximum < proverka:
+         maximum = proverka
+    mnogo.append(maximum)
+bolshoi = max(mnogo)
+
+flag = False
 for i in range(n):
    for j in range(m):
-      mult[i][j] = i * j
-      print(mult[i][j], end = ' ')
-   print()
+      if s[i][j] == str(bolshoi):
+         print(str(i) + ' ' + str(j))
+         flag = True
+         break
+   if flag:
+      break
+         
+
