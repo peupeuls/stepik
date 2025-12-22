f4074286db07d948c6c69706bd59e9cc9047e848
peupeuls <msona6261@gmail.com>
2025-12-23 02:22:29 +0500

    проверяет симметричность квадратной матрицы относительно главной диагонали

--- stepik.py
+++ stepik.py
@@ -1,15 +1,19 @@
 n = int(input())
-m = int(input())
 s=[]
 for i in range(n):
     s.append(str(input()).split())
-columns = str(input()).split()
-col1 = int(columns[0])
-col2 = int(columns[1])
 
+flag = True
 for i in range(n):
-    s[i][col1], s[i][col2] = s[i][col2],s[i][col1]
-for i in range(n):
-    for j in range(m):
-        print(s[i][j], end = ' ')
-    print()
+    for j in range(n):
+        if s[i][j] != s[j][i]:
+            print('NO')
+            flag = False
+            break
+    if not flag:
+        break        
+if flag:
+    print('YES')
+        
+         
+      
