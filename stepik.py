41d4fd0125098149c6be07e71798ce20993d1c3b
peupeuls <msona6261@gmail.com>
2025-12-25 00:59:19 +0500

    поворачивает квадратную матрицу чисел на 90∘ по часовой стрелке.

--- stepik.py
+++ stepik.py
@@ -3,11 +3,11 @@ s = []
 for i in range (n):
    s.append(str(input()).split())
 
-for i in range(n//2):
+'''for i in range(n):
    for j in range(n):
-      s[i][j], s[n-1-i][j] = s[n-1-i][j], s[i][j]
+      s[i][j]'''
 
 for i in range(n):
    for j in range(n):
-      print(s[i][j], end = ' ')
+      print(s[n-1-j][i], end = ' ')
    print()
