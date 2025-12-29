2f535c356c0e9bbab6f5833d6f63a5a254a912e9
peupeuls <msona6261@gmail.com>
2025-12-30 01:02:06 +0500

    Напишите программу для создания матрицы размером n × m, заполнив ее символами . и * в шахматном порядке. В левом верхнем углу должна стоять точка.

--- stepik.py
+++ stepik.py
@@ -1,15 +1,17 @@
-n = int(input())
+chisla = input().split()
+n = int(chisla[0])
+m = int(chisla[1])
+
 for i in range (n):
-    s = [[0] * n for _ in range(n)]
+    s = [[0] * m for _ in range(n)]
 
 for i in range(n):
-    for j in range(n):
-        if j == n - i - 1:
-            s[i][j] = 1
-        elif j > n - i - 1:
-            s[i][j] = 2
-
+    for j in range(m):
+        if (i + j)%2 == 0:
+            s[i][j] = "."
+        else:
+            s[i][j] = "*"
 for i in range(n):
-    for j in range(n):
+    for j in range(m):
         print(s[i][j], end = ' ')
     print()
