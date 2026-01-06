96d77f4bc36c715686316d69a92818a9c6122217
peupeuls <msona6261@gmail.com>
2026-01-06 20:03:52 +0500

    создает матрицу размером n × m, заполнив ее "змейкой"

--- stepik.py
+++ stepik.py
@@ -1,12 +1,22 @@
-n = int(input())
-for i in range (n):
-    s = [[0] * n for _ in range(n)]
+chisla = input().split()
+n = int(chisla[0])
+m = int(chisla[1])
+
+
+s = [[0] * m for _ in range (n)] 
+
+step = 1
+flag = True
+for i in range(n):
+    for j in range(m):
+        if i%2 == 0:
+            s[i][j] = step
+            step += 1
+        if i%2 == 1:
+            s[i][m-j-1] = step
+            step += 1
 
 for i in range(n):
-    for j in range(n):
-        if (i == j) | (j == n - i -1):
-            s[i][j] = 1
-for i in range(n):
-    for j in range(n):
+    for j in range(m):
         print(s[i][j], end = ' ')
     print()
