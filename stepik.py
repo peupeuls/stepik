a407a39d5086af877589c711b0b3d6a6ab93e665
peupeuls <msona6261@gmail.com>
2026-01-11 10:30:49 +0500

    snowflake

--- stepik.py
+++ stepik.py
@@ -1,9 +1,19 @@
 n = int(input())
-s=[]
+s = [ ['.'] * n for _ in range(n)]
+
+seredina = (n-1)/2
 for i in range(n):
-    s.append(str(input()).split())
+    for j in range(n):
+        if i == j:
+            s[i][j] = '*'
+        elif i == n - j - 1:
+            s[i][j] = '*'
+        elif i == seredina:
+            s[i][j] = '*'
+        elif j == seredina:
+            s[i][j] = '*'
 
 for i in range(n):
     for j in range(n):
-        print(s[j][i], end = ' ')
+        print(s[i][j], end = ' ')
     print()
