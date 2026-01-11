9b4b0dadb97d71b4e028aa5f414da416889ac9ea
peupeuls <msona6261@gmail.com>
2026-01-11 10:18:42 +0500

    транспонирование матрицы

--- stepik.py
+++ stepik.py
@@ -3,15 +3,7 @@ s=[]
 for i in range(n):
     s.append(str(input()).split())
 
-maximum = 0
-proverka = 0
 for i in range(n):
     for j in range(n):
-     if i >= n - j - 1:
-      proverka = int(s[i][j])
-      if maximum == 0 or maximum < proverka:
-         maximum = proverka
-
-
-print(maximum)
-        
+        print(s[j][i], end = ' ')
+    print()
