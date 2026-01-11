35fdcd85245c5ae390ff4b3cff931d9e20a760d2
peupeuls <msona6261@gmail.com>
2026-01-11 10:07:59 +0500

    разделяет список на вложенные подсписки так, что n последовательных элементов принадлежат разным подспискам.

--- stepik.py
+++ stepik.py
@@ -1,29 +1,10 @@
-chisla1 = input().split()
-n = int(chisla1[0])
-m = int(chisla1[1])
+stroka = input().split()
+n = int(input())
+dlina = len(stroka)
 
-matrix1 = []
-for i in range(n):
-    matrix1.append(input().split())
-    
-probel = input()
-
-chisla2 = input().split()
-k = int(chisla2[0])
-p = int(chisla2[1])
-
-matrix2 = []
-for i in range(k):
-    matrix2.append(input().split())
-
-umn = [[0] * p for _ in range(n)]
-
-for i in range(n):
-    for j in range(p):
-        for l in range(m):
-            umn[i][j]+= int(matrix1[i][l]) * int(matrix2[l][j])
-
-for i in range(n):
-    for j in range(p):
-        print(umn[i][j], end = ' ')
-    print()
+result = []
+i = 0
+while i < n:
+   result.append(stroka[i::n])
+   i += 1
+print(result)
