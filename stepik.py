30a00cb89964db18aade7a2fecc067e2651ff5a8
peupeuls <msona6261@gmail.com>
2026-01-11 14:45:40 +0500

    латинский квадрат

--- stepik.py
+++ stepik.py
@@ -3,10 +3,12 @@ s=[]
 for i in range(n):
     s.append(str(input()).split())
 
+
 flag = True
+
 for i in range(n):
-    for j in range(n):
-        if s[i][j] != s[n - j - 1][n - i - 1]:
+    for j in range(1, n + 1):
+        if str(j) not in s[i] or str(j) not in [s[k][i] for k in range(n)]:
             print('NO')
             flag = False
             break
@@ -14,3 +16,4 @@ for i in range(n):
         break
 if flag:
     print('YES')
+
