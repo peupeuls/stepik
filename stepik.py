53042474644af089a3e21fbb46ffb8ae35b11225
peupeuls <msona6261@gmail.com>
2026-01-11 10:52:38 +0500

    симметричность относительно побочной

--- stepik.py
+++ stepik.py
@@ -1,19 +1,16 @@
 n = int(input())
-s = [ ['.'] * n for _ in range(n)]
+s=[]
+for i in range(n):
+    s.append(str(input()).split())
 
-seredina = (n-1)/2
+flag = True
 for i in range(n):
     for j in range(n):
-        if i == j:
-            s[i][j] = '*'
-        elif i == n - j - 1:
-            s[i][j] = '*'
-        elif i == seredina:
-            s[i][j] = '*'
-        elif j == seredina:
-            s[i][j] = '*'
-
-for i in range(n):
-    for j in range(n):
-        print(s[i][j], end = ' ')
-    print()
+        if s[i][j] != s[n - j - 1][n - i - 1]:
+            print('NO')
+            flag = False
+            break
+    if not flag:
+        break
+if flag:
+    print('YES')
