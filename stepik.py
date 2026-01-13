0463c9bf8ea09a419be4350afd5d204cf75b8d1c
peupeuls <msona6261@gmail.com>
2026-01-13 08:06:34 +0500

    проверяет и выводит список учеников а затем список хорошистов и отличников

--- stepik.py
+++ stepik.py
@@ -1,9 +1,12 @@
-tuples = [(10, 20, 40), (40, 50, 60), (70, 80, 90), (10, 90), (1, 2, 3, 4), (5, 6, 10, 2, 1, 77)]
-new_tuples = []
-n = len(tuples)
+n = int(input())
+spisok = []
 for i in range(n):
-    a = list(tuples[i])
-    a[-1] = 100
-    b = tuple(a)
-    new_tuples.append(b)
-print(new_tuples)
+    spisok.append(input())
+
+for i in range(n):
+    print(spisok[i])
+print()
+
+for item in spisok:
+    if '4' in item or '5' in item:
+        print(item)
