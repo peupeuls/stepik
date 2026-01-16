71c9b34d5e0cb940d2157ad8c565959522a7ad40
peupeuls <msona6261@gmail.com>
2026-01-17 03:12:50 +0500

    Напишите программу, выводящую множество оценок, которые есть и у первого, и у второго учеников, но которых нет у третьего ученика.

--- stepik.py
+++ stepik.py
@@ -1,7 +1,7 @@
-set1 = set(input())
-set2 = set(input())
-set3 = set1 & set2
-if set3 == set2:
-    print("YES")
-else:
-    print("NO")
+set1 = input().split()
+set2 = input().split()
+set3 = input().split()
+set4 = set(set1) & set(set2)
+set5 = set(set4) - set(set3)
+for elem in sorted(set5, reverse=True, key = int):
+    print(elem, end = " ")
