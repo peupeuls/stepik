ebe85dab5f288dcbf839e5e3cd443c8b4b65e722
peupeuls <msona6261@gmail.com>
2026-01-17 03:15:42 +0500

    Напишите программу, которая выводит такие оценки, которые встречаются не более, чем у двух учеников.

--- stepik.py
+++ stepik.py
@@ -1,7 +1,7 @@
 set1 = input().split()
 set2 = input().split()
 set3 = input().split()
-set4 = set(set1) & set(set2)
-set5 = set(set4) - set(set3)
-for elem in sorted(set5, reverse=True, key = int):
+set4 = set(set1) & set(set2) & set(set3)
+set5 = (set(set1) | set(set2) | set(set3)) - set(set4)
+for elem in sorted(set5, key = int):
     print(elem, end = " ")
