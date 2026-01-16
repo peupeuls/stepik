1c997ee0836e82d5df5dd3a7a813a31f8fe2a3c3
peupeuls <msona6261@gmail.com>
2026-01-16 23:24:03 +0500

    Напишите программу для определения общего количества различных слов в строке текста.

--- stepik.py
+++ stepik.py
@@ -1,7 +1,12 @@
-n = int(input())
-slova = []
-for i in range(n):
-    slova.append(input())
-vmeste = ''.join(slova)
-s = len(set(vmeste.lower()))
-print(s)
+# n = set(input().replace(' ', ''))
+# m = set(input().replace(' ', ''))
+
+# s = n & m
+# print(s)
+n = (input().lower()).split()
+s = set()
+for i in range(len(n)):
+    new_n = n[i].strip('.,;:-?!')
+    s.add(new_n)
+
+print(len(s))
