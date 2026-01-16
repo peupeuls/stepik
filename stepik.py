fcfe21bd130f25a8f49cf875c58f9b5a58e5b84c
peupeuls <msona6261@gmail.com>
2026-01-17 00:19:29 +0500

    выводит все числа в порядке возрастания, которые есть как в первой строке, так и во второй.

--- stepik.py
+++ stepik.py
@@ -3,6 +3,8 @@ m = input().split()
 
 set1 = set(n)
 set2 = set(m)
-print(len(set1 & set2))
-
+set3 = set1 & set2
+massiv = sorted(list(set3), key = int)
+for elem in massiv:
+    print(elem, end = " ")
 
