2bb2f1a07d29342d693f4c480a9b759254552603
peupeuls <msona6261@gmail.com>
2026-01-18 01:58:42 +0500

    Напишите программу, считывающую информацию об игре и сообщающую ребятам, что очередной город назван повторно.

--- stepik.py
+++ stepik.py
@@ -1,4 +1,7 @@
-files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
-files1 = [s.lower() for s in files]
-set1 = {n for n in files1 if 'png' in n}
-print(*sorted(set1))
+n = int(input())
+list1 = [input() for _ in range(n+1)]
+set1 = set(list1)
+if len(list1) == len(set1):
+    print('OK')
+else:
+    print('REPEAT')
