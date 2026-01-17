bbe93b6a20b2667057bf762703f1d0be96ef2a29
peupeuls <msona6261@gmail.com>
2026-01-18 02:11:39 +0500

    При приёме новых сотрудников в онлайн-школу BEEGEEK ее руководитель тестирует не только профессиональные качества кандидата, но и его память.
    Кандидату показывают ненадолго несколько различных чисел, а затем кандидат должен их назвать. Причем неважно, в каком порядке он их вспоминает, и повторяется он или нет, главное он должен назвать все числа, не добавляя лишних.
    
    Напишите программу, определяющую, успешно ли прошел кандидат тестирование памяти.

--- stepik.py
+++ stepik.py
@@ -1,7 +1,6 @@
 set1 = set(input().split())
 set2 = set(input().split())
-set3 = set1 & set2
-if set3:
-    print(*sorted(set3, reverse = True, key = int))
-if not set3:
-    print('BAD DAY')
+if set1 == set2:
+    print('YES')
+else:
+    print('NO')
