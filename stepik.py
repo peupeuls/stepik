3e5b6559b87f842dda73de4b7cf5eea8ed162be1
peupeuls <msona6261@gmail.com>
2026-01-18 02:03:37 +0500

    Напишите программу, определяющую для каждой книги из списка на прочтение, есть она у Руслана или нет.

--- stepik.py
+++ stepik.py
@@ -1,7 +1,10 @@
+m = int(input())
 n = int(input())
-list1 = [input() for _ in range(n+1)]
-set1 = set(list1)
-if len(list1) == len(set1):
-    print('OK')
-else:
-    print('REPEAT')
+
+dom = [input() for _ in range(m)]
+spis = [input() for _ in range(n)]
+for i in range(n):
+    if spis[i] in dom:
+        print('YES')
+    else:
+        print('NO')
