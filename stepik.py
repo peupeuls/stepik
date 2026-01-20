3729c7fef1e3774754ac6a04fffa93a86eba5805
peupeuls <msona6261@gmail.com>
2026-01-21 01:43:18 +0500

    Дополните приведенный ниже код так, чтобы он объединил содержимое двух словарей dict1 и dict2: если ключ есть в обоих словарях, добавьте его в результирующий словарь со значением, равным сумме соответствующих значений из первого и второго словаря; если ключ есть только в одном из словарей, добавьте его в результирующий словарь с его текущим значением. Результирующий словарь необходимо присвоить переменной result.

--- stepik.py
+++ stepik.py
@@ -1,3 +1,11 @@
-result = {}
-for i in range(1,16):
-        result[i] = i**2
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+
+result = dict2.copy()
+for key in dict1.keys():
+    if key not in dict2.keys():
+        result[key] = dict1[key]
+    if key in dict2.keys():
+        result[key] = dict1[key] + dict2[key]
+
+print(result)
