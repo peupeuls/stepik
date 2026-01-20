8d85028d4f195fc636031c686ae051b3e4ba3a37
peupeuls <msona6261@gmail.com>
2026-01-21 00:57:41 +0500

    упрощенный код про количество цифр

--- stepik.py
+++ stepik.py
@@ -2,8 +2,4 @@ numbers = [9, 8, 32, 1, 10, 1, 10, 23, 1, 4, 10, 4, 2, 2, 2, 2, 1, 10, 1, 2, 2, 
 
 result = {}
 for num in numbers:
-    if num not in result:
-        result[num] = 1
-    else:
-        result[num] += 1
-print(result)
+    result[num] = result.get(num, 0) + 1
