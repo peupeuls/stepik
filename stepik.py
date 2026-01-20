f6ab3bd7de6c9ce64b1cce8ab179e334b7aca1de
peupeuls <msona6261@gmail.com>
2026-01-21 02:03:02 +0500

    Дополните приведенный ниже код так, чтобы он вывел наиболее часто встречающееся слово строки s. Если таких слов несколько, должно быть выведено то, что меньше в лексикографическом порядке.

--- stepik.py
+++ stepik.py
@@ -1,8 +1,15 @@
-text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
-
-result = {}
-for w in text:
-    if w not in result:
-        result[w] = 1
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+ss = s.split()
+r = {}
+for w in ss:
+    if w not in r:
+        r[w] = 1
     else:
-        result[w] += 1
+        r[w] += 1
+
+m = max(r.values())
+n = []
+for key in r.keys():
+    if r[key] == m:
+        n.append(key)
+print(min(n))
