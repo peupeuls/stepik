4dba3e84bf144aee98e100776ed9a6c732dbcae4
peupeuls <msona6261@gmail.com>
2026-01-21 01:45:11 +0500

    Дополните приведенный ниже код так, чтобы в переменной result хранился словарь, в котором для каждого символа строки text будет подсчитано количество его вхождений.

--- stepik.py
+++ stepik.py
@@ -1,11 +1,8 @@
-dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
-dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
 
-result = dict2.copy()
-for key in dict1.keys():
-    if key not in dict2.keys():
-        result[key] = dict1[key]
-    if key in dict2.keys():
-        result[key] = dict1[key] + dict2[key]
-
-print(result)
+result = {}
+for w in text:
+    if w not in result:
+        result[w] = 1
+    else:
+        result[w] += 1
