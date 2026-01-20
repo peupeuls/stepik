1108ad5947a05601d94326f6774ac1211cbbd379
peupeuls <msona6261@gmail.com>
2026-01-21 01:32:30 +0500

    в переменной result хранился словарь, в котором ключи – числа от 1 до 15 (включительно), а значения представляют собой квадраты ключей.

--- stepik.py
+++ stepik.py
@@ -1,5 +1,3 @@
-numbers = [9, 8, 32, 1, 10, 1, 10, 23, 1, 4, 10, 4, 2, 2, 2, 2, 1, 10, 1, 2, 2, 32, 23, 23]
-
 result = {}
-for num in numbers:
-    result[num] = result.get(num, 0) + 1
+for i in range(1,16):
+        result[i] = i**2
