faca3f8508fe3424d4e3019ebd95421fd82cf6ef
peupeuls <msona6261@gmail.com>
2026-01-22 07:53:05 +0500

    Дополните приведенный код, используя генератор, так, чтобы получить словарь result, состоящий из всех элементов словаря colors, кроме тех, у которых значением является None.

--- stepik.py
+++ stepik.py
@@ -1,8 +1,6 @@
-n = int(input())
-sl = {}
-for i in range(n):
-    key, value = input().split()
-    sl[key] = value
-    sl[value] = key
-m = input()
-print(sl[m])
+colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink', 'c7': 'Orange', 'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet', 'c12': 'Gold', 'c13': None, 'c14': 'Amber', 'c15': 'Azure', 'c16': 'Beige', 'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None, 'c22': 'Sand', 'c23': None}
+
+result = {c: colors[c] for c in colors if colors[c] != None}
+
+
+
