81fecdaee9d9e0585916069ac80fd8d3d51e441a
peupeuls <msona6261@gmail.com>
2026-01-22 08:02:12 +0500

    В переменной s хранится строка пар число:слово. Дополните приведенный код, используя генератор, так, чтобы получить словарь result , в котором числа будут ключами, а слова – значениями.

--- stepik.py
+++ stepik.py
@@ -1,6 +1,7 @@
-colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink', 'c7': 'Orange', 'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet', 'c12': 'Gold', 'c13': None, 'c14': 'Amber', 'c15': 'Azure', 'c16': 'Beige', 'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None, 'c22': 'Sand', 'c23': None}
+s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
 
-result = {c: colors[c] for c in colors if colors[c] != None}
-
-
-
+result = {}
+ss = s.split()
+for i in range(len(ss)):
+    key, value = ss[i].split(':')
+    result[int(key)] = value
